edbbafd7d2751f9af7355f7674e10ca864db1dbe
Elizabeth Renderos <elirenderos@gmail.com>
2023-01-01 13:40:33 +0900

    added additional attributes

--- src/components/info.js
+++ src/components/info.js
@@ -7,7 +7,7 @@ function Info() {
   return (
     <main className="card-info">
       <div className="main-info">
-        <img className="profile-pic" src={Img} />
+        <img alt="profilepic"className="profile-pic" src={Img} />
         <h1 className="name-info">Elizabeth Renderos</h1>
         <h2 className="job-info">Frontend Developer</h2>
         <h3 className="email-info">elirenderos@gmail.com</h3>
@@ -19,7 +19,7 @@ function Info() {
             Email
           </a>
         </button>
-        <a href="https://linkedin.com/in/elizabeth27283" target={'_blank'} className="linkedIn-text">
+        <a href="https://linkedin.com/in/elizabeth27283" target={'_blank'}  rel="noreferrer" className="linkedIn-text">
           <button className="linkedIn-button-info">
             <AiFillLinkedin fontSize={"14px"} />
             LinkedIn
